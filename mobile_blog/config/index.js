b2be0c7003e40ccc0b82c5a6b1506c636c39d50f
������ <shanll@dianzhong.com>
2019-07-23 17:44:26 +0800

    移动端history模式

--- mobile_blog/config/index.js
+++ mobile_blog/config/index.js
@@ -51,7 +51,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../mobile_front'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
